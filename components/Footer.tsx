344cae6e0aace62cb8dcace6bc62d45f8d94fdf2
AbderrahimAl <abderrahimalakouche@gmail.com>
2024-03-11 22:08:53 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,9 +19,11 @@ export default function Footer() {
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
-          <Link href="https://github.com/AbderrahimAl/Blog">
-            Open Source
-          </Link>
+          <div>
+            <Link href="https://github.com/AbderrahimAl/Blog">
+              Open Source
+            </Link>
+          </div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
         </div>
