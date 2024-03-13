fee0b37df73644355ee3858b24c0fb679fec1743
AbderrahimAl <abderrahimalakouche@gmail.com>
2024-03-13 14:24:32 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,26 @@
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
+import SocialIcon from '@/components/social-icons'
 
 export default function Footer() {
   return (
     <footer>
-      <div className="flex flex-col items-center mt-16">
-        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+      <div className="mt-16 flex flex-col items-center">
+        <div className="mb-3 flex space-x-4">
+          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
+          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
+          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
+          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+        </div>
+        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            <div>Theme by timlrx</div>
-          </Link>
-        </div>
-        <div className="flex mb-8 space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/AbderrahimAl/Blog/"> Open Source </Link>
-          <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
+        </div>
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
+            Tailwind Nextjs Theme
+          </Link>
         </div>
       </div>
     </footer>
